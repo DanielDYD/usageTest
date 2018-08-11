c110fc7f69e050aedf04bbdbd6a7529f7a890869
Daniel <duan.yun.d@foxmail.com>
2018-08-12 00:01:10 +0800

    normalize storing way

---
+++ src/usageTest.cpp
@@ -0,0 +1,29 @@
+//============================================================================
+// Name        : usageTest.cpp
+// Author      : Duan Yunde
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+//Ã¯‘æ”Œœ∑
+#include<iostream>
+
+using namespace std;
+
+
+
+int main(int argc, char **argv) {
+    char *p;
+    cout << sizeof(p);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
